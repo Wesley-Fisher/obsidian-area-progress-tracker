24202a88018bd106b4d916d4e51ec867a77cb430
Wesley Fisher <wesley_b_fisher@hotmail.com>
2026-03-29 10:29:50 -0300

    Update script to re-load JSON files for testing

--- prepareTestRepo.ps1
+++ prepareTestRepo.ps1
@@ -1,2 +1,7 @@
+npm run build
+
 Copy-Item ".\dist\main.js" -Destination ".\\test-vault-area-progress-tracker\\.obsidian\\plugins\\area-progress-tracker\\" -Force
 Copy-Item ".\manifest.json" -Destination ".\\test-vault-area-progress-tracker\\.obsidian\\plugins\\area-progress-tracker\\" -Force
+
+Copy-Item ".\\test-vault-area-progress-tracker\\ProgressTrackerSource\\*.json" -Destination ".\\test-vault-area-progress-tracker\\ProgressTracker\\" -Force
+Copy-Item ".\\test-vault-area-progress-tracker\\ProgressTrackerSource\\logs\\*.json" -Destination ".\\test-vault-area-progress-tracker\\ProgressTracker\\logs" -Force
